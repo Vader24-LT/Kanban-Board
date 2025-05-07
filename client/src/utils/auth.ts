03bbf5dd65d224ecddf32a0ac9a4347721b07a68
Your GitHub Username <your-email@example.com>
2025-05-07 00:08:31 -0500

    push

---
+++ client/src/utils/auth.ts
@@ -0,0 +1,53 @@
+import { JwtPayload, jwtDecode } from 'jwt-decode';
+
+class AuthService {
+  getProfile() {
+    // TODO: return the decoded token
+    const token = this.getToken();
+    return token ? jwtDecode<JwtPayload>(token) : null;
+  }
+
+  loggedIn() {
+    // TODO: return a value that indicates if the user is logged in
+    const token = this.getToken();
+    return !!token && !this.isTokenExpired(token);
+  }
+
+  isTokenExpired(token: string) {
+    // TODO: return a value that indicates if the token is expired
+    try {
+      const decoded = jwtDecode<JwtPayload>(token);
+      const currentTime = Date.now() / 1000;
+
+      if (!decoded.exp) {
+        throw new Error('Token does not contain an expiration time');
+      }
+
+      return decoded.exp < currentTime;
+    } catch (error) {
+      console.error(error);
+      return false;
+    }
+  }
+
+  getToken(): string {
+    // TODO: return the token
+    return localStorage.getItem('id_token') || '';
+  }
+
+  login(idToken: string) {
+    // TODO: set the token to localStorage
+    // TODO: redirect to the home page
+    localStorage.setItem('id_token', idToken);
+    window.location.assign('/');
+  }
+
+  logout() {
+    // TODO: remove the token from localStorage
+    // TODO: redirect to the login page
+    localStorage.removeItem('id_token');
+    window.location.assign('/login');
+  }
+}
+
+export default new AuthService();
